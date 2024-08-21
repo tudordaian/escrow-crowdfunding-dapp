eff8cc6418db09de01700b94b314e5924a18b481
Tudor Daian <tudor.daian@student.tuiasi.ro>
2024-08-22 01:34:50 +0300

    escrow dapp

--- src/utils/smartContract.ts
+++
@@ -1,9 +0,0 @@
-import json from 'contracts/ping-pong.abi.json';
-import { AbiRegistry, Address, SmartContract } from './sdkDappCore';
-
-const abi = AbiRegistry.create(json);
-
-export const smartContract = new SmartContract({
-    address: new Address(contractAddress),
-    abi
-});
